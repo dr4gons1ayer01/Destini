b9da23e9404e03ba4a254fb190e93df94a37e19e
Ivan Semenov <izzuyinvest@gmail.com>
2024-02-22 20:59:59 +0300

    Implemented logic for story transitions

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -9,12 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     let mainView = MainView()
+    var storyBrain = StoryBrain()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view = mainView
+        updateUI()
+        
+        tap1Choice()
+        tap2Choice()
     }
-
-
+    func updateUI() {
+        let story = storyBrain.stories[storyBrain.storyNumber]
+        mainView.storyLabel.text = story.title
+        mainView.choice1Button.setTitle(story.choice1, for: .normal)
+        mainView.choice2Button.setTitle(story.choice2, for: .normal)
+    }
+    func choiceMade(destination: Int) {
+        storyBrain.nextStory(destination)
+        updateUI()
+    }
+    
+    func tap1Choice() {
+        let tap = UIAction { _ in
+            self.choiceMade(destination: 1)
+        }
+        mainView.choice1Button.addAction(tap, for: .touchUpInside)
+    }
+    func tap2Choice() {
+        let tap = UIAction { _ in
+            self.choiceMade(destination: 2)
+        }
+        mainView.choice2Button.addAction(tap, for: .touchUpInside)
+    }
 }
 
--- Destini/View/MainView.swift
+++ Destini/View/MainView.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class MainView: UIView {
     let backgroundImage = UIImageView(image: UIImage(named: "background"))
+    let storyLabel = UILabel()
+    let choice1Button = UIButton(title: "выбор 1", bg: UIColor(named: "choice1bg")!)
+    let choice2Button = UIButton(title: "выбор 2", bg: UIColor(named: "choice2bg")!)
     
     init() {
         super.init(frame: CGRect())
@@ -16,23 +19,61 @@ class MainView: UIView {
     }
     func setupUI() {
         backgroundImage.contentMode = .scaleAspectFill
+        storyLabel.textAlignment = .center
+        storyLabel.lineBreakMode = .byWordWrapping        
+        storyLabel.numberOfLines = 16
+        storyLabel.font = UIFont(name: "Gilroy-Lightitalic", size: 25)
+        storyLabel.textColor = .white
+        
+        storyLabel.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        storyLabel.layer.cornerRadius = 12
+        storyLabel.clipsToBounds = true
+
+        let stack = UIStackView(arrangedSubviews: [storyLabel, choice1Button, choice2Button])
+        stack.axis = .vertical
+        stack.spacing = 20
         
         addSubview(backgroundImage)
+        addSubview(stack)
         backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+        stack.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             backgroundImage.topAnchor.constraint(equalTo: topAnchor),
             backgroundImage.centerXAnchor.constraint(equalTo: centerXAnchor),
             backgroundImage.leadingAnchor.constraint(equalTo: leadingAnchor),
             backgroundImage.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            stack.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            stack.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stack.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            stack.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            
+            storyLabel.bottomAnchor.constraint(equalTo: choice1Button.topAnchor, constant: -80),
         ])
-        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+extension UIButton {
+    convenience init(title: String, bg: UIColor) {
+        self.init(type: .system)
+        setTitle(title, for: .normal)
+        setTitleColor(.white, for: .normal)
+        titleLabel?.font = UIFont(name: "Gilroy-Lightitalic", size: 25)
+        backgroundColor = bg
+        titleLabel?.numberOfLines = 0
+        titleLabel?.lineBreakMode = .byWordWrapping
+        titleLabel?.textAlignment = .center
+        layer.cornerRadius = 12
+        layer.borderWidth = 2
+        layer.borderColor = UIColor(white: 1, alpha: 0.6).cgColor
+        heightAnchor.constraint(equalToConstant: 70).isActive = true
+    }
+}
+
 import SwiftUI
 
 struct MainViewProvider: PreviewProvider {
